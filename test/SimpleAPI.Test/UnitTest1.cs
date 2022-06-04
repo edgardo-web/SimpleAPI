16d996652c5093007d9b01f88160ec0c04a232a3
Edgardo Garcia <edgardo.a.garcia.f@gmail.com>
2022-06-03 21:36:30 -0500

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using SimpleAPI.Controllers;
+
+
+namespace SimpleAPI.Test;
+
+public class UnitTest1
+{
+    WeatherForecastController controller = new WeatherForecastController();
+
+    [Fact]
+    public void Test1()
+    {
+        var returnvalue = controller.Get();
+        Assert.NotNull(returnvalue);
+    }
+}
